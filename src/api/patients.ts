ebe0bb00bc38a80bc482cccc193f1c07356bc11c
Tagir Sharipov <tsharipov@keybiz.it>
2023-06-20 18:08:48 +0200

    fixed api url

--- src/api/patients.ts
+++ src/api/patients.ts
@@ -4,21 +4,21 @@ const headers = new Headers({
    "Authorization": `Basic ${btoa(`${process.env.REACT_APP_USERNAME}:${process.env.REACT_APP_PASSWORD}`)}`,
    'Content-Type': 'application/json',
  })
- 
+
 export function fetchPatients() {
-   return fetch("https://mobile.digistat.it/CandidateApi/Patient/GetList",  {
+   return fetch(`${process.env.REACT_APP_API}Patient/GetList`, {
       headers,
    });
 }
 
 export function  fetchPatient(id:string) {
-   return fetch(`https://mobile.digistat.it/CandidateApi/Patient/Get/${id}`,  {
+   return fetch(`${process.env.REACT_APP_API}Patient/Get/${id}`,  {
       headers,
    });
 }
 
 export async function postPatient(patient: Patient) {
-   return await fetch(`https://mobile.digistat.it/CandidateApi/Patient/Update`,  {
+   return await fetch(`${process.env.REACT_APP_API}Patient/Update`,  {
       method: 'POST',
       headers,
       body: JSON.stringify(patient),
